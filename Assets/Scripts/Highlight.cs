40f1fc3ec1f6d6a696a384b695643cf293ca4500
eb00t <eoghanjtoreilly1@gmail.com>
2023-04-28 16:05:29 +0100

    Small fixes

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    public Material HighlightShader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        //gameObject= HighlightShader;
+    }
+}
